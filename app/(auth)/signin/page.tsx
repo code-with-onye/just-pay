5b17e9b3b682d2e53036377839a6d81cdc7bcf5d
code-with-onye <onyemachitrust@gmail.com>
2024-03-31 01:27:04 +0100

    changes

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,10 +1,11 @@
+import { Suspense } from "react";
 import { SignInSection } from "./_components/signin-section";
 
 export default function SiginInPage() {
     return (
-      <>
+      <Suspense fallback={<div>Loading...</div>}>
         <SignInSection />
-      </>
+      </Suspense>
     );
   }
   
